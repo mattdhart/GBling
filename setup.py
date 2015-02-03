54d72dde69768e8bb60d169918a2b18c300af424
Matt Hart <mdhart@amazon.com>
2015-02-03 09:24:39 -0800

    Test and doc scaffolding

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'NBA GBling',
+    'author': 'mdhart',
+    'author_email': 'mattdhart@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['nba'],
+    'scripts': [],
+    'name': 'nba'
+}
+
+setup(**config)
+
